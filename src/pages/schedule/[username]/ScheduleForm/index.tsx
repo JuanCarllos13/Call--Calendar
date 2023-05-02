c1fcd39e9f17c75307299aada832e15221b56066
Juan Carlos <juan2017amaral@gmail.com>
2023-05-02 17:53:10 -0400

    test

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { CalendarStep } from "./CalendarStep";
 import { ConfirmStep } from "./ConfirmStep";
 
-export function ScheduleForm(){
-  return (
-    <CalendarStep/>
-  )
+export function ScheduleForm() {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>();
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null);
+  }
+
+  if (selectedDateTime) {
+    return <ConfirmStep />;
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />;
 }
