fc554a08b357a71ef5d7d7183c7c3dd97c32407a
Juan Carlos <juan2017amaral@gmail.com>
2023-05-02 17:31:36 -0400

    finish validation calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,54 +1,54 @@
-import { useQuery } from '@tanstack/react-query'
-import dayjs from 'dayjs'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { Calendar } from '../../../../../components/Calendar'
-import { api } from '../../../../../lib/axios'
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Calendar } from "../../../../../components/Calendar";
+import { api } from "../../../../../lib/axios";
 import {
   Container,
   TimePicker,
   TimePickerHeader,
   TimePickerItem,
   TimePickerList,
-} from './styles'
+} from "./styles";
 
 interface Availability {
-  possibleTimes: number[]
-  availableTimes: number[]
+  possibleTimes: number[];
+  availableTimes: number[];
 }
 
 export function CalendarStep() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const isDateSelected = !!selectedDate
-  const username = String(router.query.username)
+  const isDateSelected = !!selectedDate;
+  const username = String(router.query.username);
 
-  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null;
   const describedDate = selectedDate
-    ? dayjs(selectedDate).format('DD[ de ]MMMM')
-    : null
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : null;
 
   const selectedDateWithoutTime = selectedDate
-    ? dayjs(selectedDate).format('YYYY-MM-DD')
-    : null
+    ? dayjs(selectedDate).format("YYYY-MM-DD")
+    : null;
 
   const { data: availability } = useQuery<Availability>(
-    ['availability', selectedDateWithoutTime],
+    ["availability", selectedDateWithoutTime],
     async () => {
       const response = await api.get(`/users/${username}/availability`, {
         params: {
           date: selectedDateWithoutTime,
         },
-      })
+      });
 
-      return response.data
+      return response.data;
     },
     {
       enabled: !!selectedDate,
-    },
-  )
+    }
+  );
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
