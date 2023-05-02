8b78407e4c2fe5050591fd0707b28ca81b72df44
Juan Carlos <juan2017amaral@gmail.com>
2023-05-02 13:41:30 -0400

    config

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -1,8 +1,8 @@
-import { prisma } from "@/lib/prisma";
 import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../lib/prisma";
 
-export default async function handle(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -11,11 +11,10 @@ export default async function handle(
   }
 
   const username = String(req.query.username);
-
   const { date } = req.query;
 
   if (!date) {
-    return res.status(400).json({ message: "Date not provided" });
+    return res.status(400).json({ message: "Date no provided." });
   }
 
   const user = await prisma.user.findUnique({
@@ -38,25 +37,43 @@ export default async function handle(
   const userAvailability = await prisma.userTimeInterval.findFirst({
     where: {
       user_id: user.id,
-      week_day: referenceDate.get('day'),
+      week_day: referenceDate.get("day"),
     },
-  })
+  });
 
   if (!userAvailability) {
-    return res.json({ availability: [] })
+    return res.json({ availability: [] });
   }
 
-  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability;
 
-  const startHour = time_start_in_minutes / 60
-  const endHour = time_end_in_minutes / 60
+  const startHour = time_start_in_minutes / 60;
+  const endHour = time_end_in_minutes / 60;
 
   const possibleTimes = Array.from({ length: endHour - startHour }).map(
     (_, i) => {
-      return startHour + i
+      return startHour + i;
+    }
+  );
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
     },
-  )
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set("hour", startHour).toDate(),
+        lte: referenceDate.set("hour", endHour).toDate(),
+      },
+    },
+  });
 
-  return res.json({ possibleTimes })
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time
+    );
+  });
 
+  return res.json({ possibleTimes, availableTimes });
 }
